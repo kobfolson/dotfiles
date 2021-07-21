f214020087e728e4a26023a48c4c6ae07221c9e9
kobfolson <kobbyfolson@gmail.com>
2021-07-21 19:21:33 +0000

    add conditional for osx

--- zsh/.config/zsh/modules/term.zsh
+++ zsh/.config/zsh/modules/term.zsh
@@ -3,7 +3,7 @@
 #
 
 export TERM_PROGRAM
-if islinux && (( $PPID > 0 )); then
+if islinux || isosx && (( $PPID > 0 )); then
   TERM_PROGRAM=$(ps -fo cmd -hwcp $PPID)
   [[ ${TERM_PROGRAM[-1]} == '-' ]] && TERM_PROGRAM=${TERM_PROGRAM[0,-2]}
 elif cat /proc/1/cgroup | grep docker -qa; then
