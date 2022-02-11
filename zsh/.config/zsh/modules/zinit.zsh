90d7e97725cc881d8786eb6df37193fa61564cfc
kobfolson <kobbyfolson@gmail.com>
2022-02-11 22:39:05 +0000

    zsh stuff

--- zsh/.config/zsh/modules/zinit.zsh
+++ zsh/.config/zsh/modules/zinit.zsh
@@ -105,12 +105,12 @@ zinit wait'2' lucid light-mode if'ismac' for \
   sbin atpull"%atclone" \
   zdharma-continuum/null
 
-zinit wait'2' if'islinux' id-as'terraform' as'readurl|command' extract \
+zinit wait'2' if'islinux' id-as'terraform' as'readurl|command' extract lucid \
     dlink0'/terraform/%VERSION%/~%.*-(alpha|beta|rc).*%' \
     dlink'/terraform/%VERSION%/terraform_%VERSION%_linux_amd64.zip' \
     for https://releases.hashicorp.com/terraform/
 
-zinit wait'2' if'ismac' id-as'terraform' as'readurl|command' extract \
+zinit wait'2' if'ismac' id-as'terraform' as'readurl|command' extract lucid \
     dlink0'/terraform/%VERSION%/~%.*-(alpha|beta|rc).*%' \
     dlink'/terraform/%VERSION%/terraform_%VERSION%_darwin_amd64.zip' \
     for https://releases.hashicorp.com/terraform/
